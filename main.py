8f3ef221a2ce52ce838f40374fe4fdd6da60d079
UnsleepingDawn <1205611978@qq.com>
2025-11-03 01:30:18 +0800

    现在通讯录能够正确地被下载

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ from source_code.SMCLabCrawler.MdtableCrawler import (
     SMCLabGourpMeetingCrawler,
     SMCLabScheduleCrawler
 )
+from source_code.SMCLabCrawler.AddressBookCrawler import (
+    SMCLabAddressBookCrawler
+)
 
 if __name__ == "__main__":
     # smclab_wr_client = SMCLabWeeklyReportCrawler()
     # smclab_wr_client.print_basic_info()
     # smclab_wr_client.get_raw_records()
-
     
     # smclab_gm_client = SMCLabGourpMeetingCrawler()
     # smclab_gm_client.print_basic_info()
     # smclab_gm_client.get_raw_records()
 
-    smclab_s_client = SMCLabScheduleCrawler()
-    smclab_s_client.print_basic_info()
-    smclab_s_client.get_raw_records() 
+    # smclab_s_client = SMCLabScheduleCrawler()
+    # smclab_s_client.print_basic_info()
+    # smclab_s_client.get_raw_records() 
+
+    smclab_ab_client = SMCLabAddressBookCrawler()
+    # smclab_ab_client.get_department_id()
+    smclab_ab_client.get_raw_records() 
--- source_code/SMCLabCrawler/AddressBookCrawler.py
+++ source_code/SMCLabCrawler/AddressBookCrawler.py
@@ -3,8 +3,9 @@ import json
 import lark_oapi as lark
 # from lark_oapi.api.bitable.v1 import *
 from lark_oapi.api.contact.v3 import *
+from lark_oapi.api.contact.v3.model.user import User
 
-from SMCLabClient import SMCLabClient
+from .SMCLabClient import SMCLabClient
 
 ABS_PATH = os.path.abspath(__file__)        # SMCLabDailyManager\source_code\SMCLabCrawler\AddressBookCrawler.py
 CURRENT_PATH = os.path.dirname(ABS_PATH)    # SMCLabDailyManager\source_code\SMCLabCrawler
@@ -18,30 +19,139 @@ class SMCLabAddressBookCrawler(SMCLabClient):
         super().__init__()
         self.page_size = page_size
         self.raw_data_path = os.path.join(RAW_DATA_PATH, "address_book_raw_data")
+        self.department_id = {}
     
-    def get_department_id_list(self):
+    def get_department_id(self, 
+                          is_update = False):
         # 参考 https://open.feishu.cn/api-explorer/cli_a8cd4e246b70d013?apiName=children&from=op_doc&project=contact&resource=department&version=v3
-        # 构造请求对象
+        raw_data_path = self.raw_data_path
+        data_path = os.path.join(raw_data_path, f"department_id.json")
+        if not is_update: # is_update为True时, 要强制下载
+            if os.path.exists(data_path):
+                with open(data_path, "r", encoding="utf-8") as f:
+                    self.department_id = json.load(f)
+                return
         has_more = True
-        page_token = None
+        page_token = ""
         page_cnt = 0
-        request: ChildrenDepartmentRequest = ChildrenDepartmentRequest.builder() \
-            .department_id("0") \
-            .user_id_type("open_id") \
-            .department_id_type("open_department_id") \
-            .fetch_child(True) \
-            .page_size(50) \
-            .build()
-
-        
+        if not os.path.exists(raw_data_path):
+            os.makedirs(raw_data_path, exist_ok=True)
         while(has_more):
             print(f"请求下载第{page_cnt}页...")
+            request: ChildrenDepartmentRequest = ChildrenDepartmentRequest.builder() \
+                .department_id("0") \
+                .user_id_type("open_id") \
+                .department_id_type("open_department_id") \
+                .fetch_child(True) \
+                .page_size(self.page_size) \
+                .page_token(page_token) \
+                .build()
             # 发起请求
-            option = lark.RequestOption.builder().user_access_token("u-deeqbJTWN2TpAPG46I0QtX0g3lwMg0WhoE00h1o22HEP").build()
-            response: ChildrenDepartmentResponse = self._client.contact.v3.department.children(request, option)
+            resp: ChildrenDepartmentResponse = self._client.contact.v3.department.children(request)
+            self._assert_resp(resp)
 
-        # 处理失败返回
-        if not response.success():
-            lark.logger.error(
-                f"client.contact.v3.department.children failed, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}, resp: \n{json.dumps(json.loads(response.raw.content), indent=4, ensure_ascii=False)}")
-            return
+            # 更新循环状态
+            has_more = resp.data.has_more
+            page_token = resp.data.page_token
+            page_cnt += 1
+        
+        assert page_cnt == 1, "还未适配多页Json逻辑, 请首先尝试增大Page size"
+
+        data = resp.data
+        for item in data.items:
+            department = {
+                "department_name": item.name,
+                "department_id": item.department_id,
+                "open_department_id": item.open_department_id,
+                "parent_department_id": item.parent_department_id,
+                "member_count": item.member_count, # 当前部门及其下属部门的用户（包含部门负责人）个数。
+                "primary_member_count": item.primary_member_count, # 当前部门及其下属部门的主属成员（即成员的主部门为当前部门）的数量。
+            }
+            self.department_id[item.name]=department
+        data_path = os.path.join(raw_data_path, f"department_id.json")
+        with open(data_path, 'w', encoding='utf-8') as f:
+            json.dump(self.department_id, f, ensure_ascii=False, indent=4)
+        
+    def get_one_department_records(self, 
+                                   department_id: str = "") -> List[User]:
+        has_more = True
+        page_token = ""
+        page_cnt = 0
+        users=[]
+        while(has_more):
+            print(f"\t请求下载第{page_cnt}页...")
+            request = FindByDepartmentUserRequest.builder() \
+                .department_id(department_id) \
+                .page_size(self.page_size) \
+                .page_token(page_token) \
+                .build()
+            
+            # 发起请求
+            resp: FindByDepartmentUserResponse = self._client.contact.v3.user.find_by_department(request)
+            self._assert_resp(resp) # 响应的合法性检查
+            
+            # # 保存页面
+            # resp_json = lark.JSON.marshal(resp.data, indent=4)
+            # resp_page_path = os.path.join(raw_data_path, f"resp_page_{str(page_cnt)}.json")
+            # with open(resp_page_path, 'w', encoding='utf-8') as f:
+            #     f.write(resp_json)
+            items = resp.data.items
+            if items:
+                users.extend(items)
+            # 更新循环状态
+            has_more = resp.data.has_more
+            page_token = resp.data.page_token
+            page_cnt += 1
+        return users
+    
+    def filter_primary_dept_users(self,
+                                  users: List[User], 
+                                  department_id: str) -> List[User]:
+        primary_users = []
+        
+        for user in users:
+            orders = user.orders
+            for order in orders:
+                if order.department_id == department_id and order.is_primary_dept:
+                    primary_users.append(user)
+                    break  # 一个用户只有一个主部门，找到后即可跳出循环
+        
+        return primary_users
+
+    def get_raw_records(self):
+        raw_data_path = self.raw_data_path
+
+        if self.department_id == {}:
+            self.get_department_id()
+        address_book = {}
+        
+        for department_name in self.department_id:
+            department = self.department_id[department_name]
+            open_department_id = department.get("open_department_id", "0")
+
+            print(f"正在处理部门: {department_name} ({open_department_id})")
+            users = self.get_one_department_records(open_department_id)
+            primary_users = self.filter_primary_dept_users(users, open_department_id)
+            address_book[department_name] = {
+                "department_id": open_department_id,
+                "department_name": department_name,
+                "member_count": department.get("member_count"),
+                "primary_members_count": department.get("primary_member_count"),
+                "primary_members": [
+                    {
+                        "name": user.name,
+                        "union_id": user.union_id,
+                        "open_id": user.open_id,
+                        "user_id": user.user_id,
+                        "email": user.email,
+                        "mobile": user.mobile,
+                        "cultivation": user.custom_attrs[0].value.option_value if user.custom_attrs else "",
+                        "mentor_id": user.custom_attrs[1].value.generic_user.id if user.custom_attrs else ""
+                    } for user in primary_users
+                ]
+            }
+        # 保存页面
+        address_book_path = os.path.join(raw_data_path, f"address_book.json")
+        with open(address_book_path, 'w', encoding='utf-8') as f:
+            json.dump(address_book, f, ensure_ascii=False, indent=4)
+        return
